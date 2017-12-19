c2ee02eb15fe60d77ce9c43b2eb58e19db1941da
mmSilva <mm.silva@outlook.com.br>
2017-12-19 03:19:05 -0300

    login mapping

--- src/test/java/Features/Login.feature
+++ src/test/java/Features/Login.feature
@@ -5,6 +5,6 @@ Feature: LoginFeature
 
   Scenario: Login with correct email and password
     Given I navigate to the login page
-    And I enter the email as "mm.silva@outlook.com.br" and the password as "teste12@"
+    And I enter the email as "marcy.ifpe@gmail.com" and the password as "teste12@"
     And I click login button
     Then I should see the My Account page
--- src/test/java/Steps/LoginStep.java
+++ src/test/java/Steps/LoginStep.java
@@ -1,22 +1,38 @@
 package Steps;
-
+import com.codeborne.selenide.Configuration;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.sql.Driver;
+
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class LoginStep {
+    public LoginStep(Driver driver) {
+        //WebDriver driver = new WebDriver();
+    }
     @Given("^I navigate to the login page$")
     public void iNavigateToTheLoginPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Configuration.browser = "chrome";
+        open("http://automationpractice.com/index.php?controller=authentication&back=my-account");
     }
 
     @And("^I enter the email as \"([^\"]*)\" and the password as \"([^\"]*)\"$")
-    public void iEnterTheEmailAsAndThePasswordAs(String arg0, String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void iEnterTheEmailAsAndThePasswordAs(String email, String password) throws Throwable {
+
+
+       //().findElement(By.id("email")).clear();
+        //getDriver().findElement(By.id("nome")).sendKeys(email);
+        //getDriver().findElement(By.id("passwd")).clear();
+       // getDriver().findElement(By.id("passwd")).sendKeys(password);
+        //getDriver().findElement(By.cssSelector("#SubmitLogin > span")).click();
+
     }
+
 
     @And("^I click login button$")
     public void iClickLoginButton() throws Throwable {
