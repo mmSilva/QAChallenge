2f724233af70f66dd92721a49021ff0b467a462b
mmSilva <mm.silva@outlook.com.br>
2017-12-19 02:33:19 -0300

    Login test case: unfinished

---
+++ src/test/java/Features/Login.feature
@@ -0,0 +1,10 @@
+Feature: LoginFeature
+  This feature details the login functionality of the e-commerce aplication
+
+  Background: Possibility to access the system
+
+  Scenario: Login with correct email and password
+    Given I navigate to the login page
+    And I enter the email as "mm.silva@outlook.com.br" and the password as "teste12@"
+    And I click login button
+    Then I should see the My Account page
---
+++ src/test/java/Steps/LoginStep.java
@@ -0,0 +1,33 @@
+package Steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class LoginStep {
+    @Given("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter the email as \"([^\"]*)\" and the password as \"([^\"]*)\"$")
+    public void iEnterTheEmailAsAndThePasswordAs(String arg0, String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click login button$")
+    public void iClickLoginButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see the My Account page$")
+    public void iShouldSeeTheMyAccountPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
